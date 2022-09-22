8fb65b077fa597d4b10dde4336b105d4e5203f77
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-09-22 10:06:06 -0700

    added license badges, generate test README file

---
+++ Develop/README.md
@@ -0,0 +1,41 @@
+
+    Title: README generator
+
+    License: [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+    Description:
+    
+    read me generator
+
+    Table Of Contents:
+    1. Installation:
+    2. Usage
+    3. Contributing
+    4. Testing
+    5. Questions
+
+
+    1. Installation
+
+    To install run the following commands:
+    
+    npm install
+
+    2. Usage:
+
+    Javascript, nodejs
+
+    3. Contributions:
+
+    contributions allowed
+
+    4. Testing:
+    
+    npm test
+
+    5. Questions:
+    E-mail: vickybedewi-95@hotmail.com
+    GitHub: github.com/elbedewi95
+
+
+    
--- Develop/markdown.js
+++ Develop/markdown.js
@@ -2,7 +2,7 @@ function markdown(responses){
     return `
     Title: ${responses.title}
 
-    License: ${responses.license}
+    License: ${renderLicense(responses)}
 
     Description:
     
@@ -10,6 +10,13 @@ function markdown(responses){
 
     Table Of Contents:
     1. Installation:
+    2. Usage
+    3. Contributing
+    4. Testing
+    5. Questions
+
+
+    1. Installation
 
     To install run the following commands:
     
@@ -27,12 +34,26 @@ function markdown(responses){
     
     ${responses.testCommand}
 
-    5. Contact Us:
+    5. Questions:
     E-mail: ${responses.email}
     GitHub: github.com/${responses.username}
 
 
     `
+}
+
+function renderLicense(responses){
+    var licenseBadge;
+switch (responses.license){
+    case "MIT": licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    break;
+    case "APACHE": licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    break;
+    case "GPL": licenseBadge = "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+    break;
+    case "none": licenseBadge = "No License"
+}
+return licenseBadge;
 }
 
 module.exports = markdown;
